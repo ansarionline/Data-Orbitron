0ead447986ffb248250dd1129a49c0530c528f5f
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-01-14 23:08:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,90 @@
 import dash
-from dash import Output, Input, html, dcc
+from dash import Output, Input, State, html, dcc
 import dash_bootstrap_components as dbc
-from plotly.graph_objects import Figure
-import plotly.graph_objects as go
 import plotly.subplots as ps
+import plotly.graph_objects as go
+from dash_resizable_panels import PanelGroup, Panel, PanelResizeHandle
 from flask import session
 import os
-from comp import panels
-fig = ps.make_subplots(1,1)
-app = dash.Dash('Data Orbitron',title='Data Orbitron',
-            external_stylesheets=[dbc.themes.COSMO],
-            suppress_callback_exceptions=True,
-            prevent_initial_callbacks=True)
+from comp import layout, panels, subplot, data, axis, figure
+
+# Create initial figure
+fig = ps.make_subplots(rows=1, cols=1, start_cell='top-left')
+app = dash.Dash('Data Orbitron',
+                title='Data Orbitron',
+                external_stylesheets=[dbc.themes.COSMO],
+                suppress_callback_exceptions=True,
+                prevent_initial_callbacks=True)
 
 app.server.secret_key = os.urandom(24)
 server = app.server
-def update_session():
-    if 'count' not in session:
-        session['count'] = 0  
-    session['count'] += 1  
 
-app.layout = html.Div([
-    dbc.Row([
-        dbc.Col(
-            html.Div([
-                panels.make_panel(None),
-                html.Div(id='panel-content', style={'margin-top': '20px'})
-            ]),
-            style={'height': '100vh', 'padding': '10px'},
-            width=3
-        ),
-        dbc.Col(
-            html.Div([
-                html.Div("Right Section", id='toolbar'),
-                dcc.Graph(
-                    id='figure-preview',
-                    figure=fig,
-                    config={'displaylogo': False, 'editable': True},
-                    style={'height': '80vh', 'padding': '10px'}
-                )
-            ]),
-            style={ 'padding': '20px'},
-            width=9
-        ),
-    ], style={'margin': '0', 'width': '100%'})
-])
-panels.register_panels_callbacks(app,fig,update_session,go)
+app.layout = html.Div(
+    PanelGroup(
+        id='panel-resizable',
+        direction='horizontal',
+        children=[
+            dcc.Store(id='tab-session',data={'counter': 0},storage_type='session'),
+                    Panel(
+                        id='panel-left',
+                        children=[
+                            dbc.Col(
+                                html.Div(
+                                    [
+                                        panels.make_panel(app, fig),
+                                        html.Div(id='panel-content', style={'margin-top': '20px'})
+                                    ]
+                                ),
+                                style={'height': '100vh', 'margin': '5px'},
+                            )
+                        ]
+                    ),
+                    PanelResizeHandle(
+                        html.Div(style={'height': '100%', 'width': '5px','backgroundColor':'lightblue'})
+                    ),
+                    Panel(
+                        id='panel-right',
+                        children=[
+                            dbc.Col(
+                                html.Div(
+                                    [
+                                        dcc.Graph(
+                                            id='figure-preview',
+                                            figure=fig,
+                                            config={'displaylogo': False, 'editable': True},
+                                            style={'height': '80vh', 'padding': '10px'}
+                                        )
+                                    ]
+                                ),
+                                style={'padding': '20px'}
+                            )
+                        ],
+                        defaultSizePercentage = 75
+                    ),
+                ],
+                style={'margin': '0', 'width': '100%'}
+            )
+)
+subplot.register_subplots(app, fig, go)
+data.register_data(app, fig)
+axis.register_axis(app, fig)
+layout.register_layout(app, fig)
+figure.register_figure(app,fig)
+panels.register_panel(app, 'data-button', 'data-div')
+panels.register_panel(app, 'subplots-button', 'subplots-div')
+panels.register_panel(app, 'axis-button', 'axis-div')
+panels.register_panel(app, 'figure-button', 'figure-div')
+panels.register_panel(app, 'dict-button', 'layout-div')
+
+@app.callback(
+    Output('tab-session', 'data'),
+    Input('tab-session', 'data'),
+    prevent_initial_call=True
+)
+def manage_session(data):
+    data = data or {'counter': 0}
+    data['counter'] += 1         
+    return data                  
+
 if __name__ == '__main__':
     app.run(debug=True)
