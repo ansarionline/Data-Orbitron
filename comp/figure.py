a231967f8fafef313308e934b70eb34eb9b1c362
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-01-17 00:14:27 +0500

    Add files via upload

---
+++ comp/figure.py
@@ -0,0 +1,126 @@
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Output, Input, State, Dash
+import plotly.graph_objects as go
+import json
+
+fonts = [
+    "Arial",
+    "Balto",
+    "Courier New",
+    "Droid Sans",
+    "Droid Serif",
+    "Droid Sans Mono",
+    "Gravitas One",
+    "Old Standard TT",
+    "Open Sans",
+    "Overpass",
+    "PT Sans Narrow",
+    "Raleway",
+    "Roboto",
+    "Times New Roman",
+    "Verdana"
+]
+def make_fig(fig):
+    figure_form = dbc.Container([
+        dbc.Row([
+            dbc.Col([
+                html.H5("Figure Settings"),
+                dbc.InputGroup([
+                    dbc.Input(
+                        id='fig-title-text',
+                        placeholder='Figure Title',
+                        style={'margin': '5px'}
+                    )
+                ]),
+                dbc.InputGroup([
+                    dbc.Select(id='fig-title-font',placeholder='Font',value='White',
+                                options = [{'label': l, 'value': l} for l in fonts]),
+                    dbc.Input('fig-title-size',placeholder='Size',
+                            type='number',min=5,step=1),
+                    dbc.Input('fig-title-color',type='color')
+                ],style={'margin': '5px'}),
+                dbc.InputGroup([
+                    dbc.Input('fig-title-x',placeholder='X',value=0.02,
+                            type='number',min=0,step=0.05,max=1),
+                    dbc.Input('fig-title-y',placeholder='Y',value=0.85,
+                            type='number',min=0,step=0.05,max=1),
+                ],style={'margin': '5px'})
+                ,
+                dbc.InputGroup([
+                    dbc.Input(
+                        id='fig-width',
+                        type='number',
+                        placeholder='Width',
+                        min=100,
+                        value=1080
+                        ),
+                    dbc.Input(
+                        id='fig-height',
+                        type='number',
+                        placeholder='Height',
+                        min=100,
+                        value=700
+                    )
+                ],style={'margin': '5px'}),
+                html.H5("Templates"),
+                dbc.Select(
+                    id='builtin-templates',
+                    options=[
+    {'label': 'None', 'value': 'none'},
+    {'label': 'Simple', 'value': 'simple_white'},
+    {'label': 'White', 'value': 'plotly_white'},
+    {'label': 'Dark', 'value': 'plotly_dark'},
+    {'label': 'Seaborn', 'value': 'seaborn'},
+    {'label': 'GGPlot', 'value': 'ggplot2'},
+    {'label': 'Presentation', 'value': 'presentation'}
+                    ],
+                    style={'margin': '5px'}
+                )
+            ])
+        ])
+    ], id='figure-div', style={'display': 'block'})
+    return figure_form
+
+
+
+def register_figure(app, fig):
+    @app.callback(
+        Output('figure-preview', 'figure', allow_duplicate=True),
+        [
+            Input('fig-title-text', 'value'),
+            Input('fig-title-font', 'value'),
+            Input('fig-title-size', 'value'),
+            Input('fig-title-color', 'value'),
+            Input('fig-title-x', 'value'),
+            Input('fig-title-y', 'value'),
+            Input('fig-width', 'value'),
+            Input('fig-height', 'value'),
+            Input('builtin-templates', 'value'),
+        ],
+        State('figure-preview', 'figure')
+    )
+    def update_figure(title, font, size, color, x, y, 
+                    width, height, template_name, fig):
+        def validate_input(value):
+            return value is not None and len(str(value)) > 0
+        def return_input(value):
+            return value if validate_input(value) else None
+        fig = go.Figure(fig)
+        layout_update = {}
+        layout_update['template'] = return_input(template_name) or fig.layout.template
+        layout_update['title'] = {
+            'text': return_input(title) or fig.layout.title.text,
+            'font': {
+                'family': return_input(font) or (fig.layout.title.font.family if fig.layout.title else 'Arial'),
+                'size': return_input(size) or (fig.layout.title.font.size if fig.layout.title else 24),
+                'color': return_input(color) or (fig.layout.title.font.color if fig.layout.title else 'black')
+            },
+            'x': return_input(x) or fig.layout.title.x,
+            'y': return_input(y) or fig.layout.title.y
+        }
+        layout_update['width'] = return_input(width) or fig.layout.width
+        layout_update['height'] = return_input(height) or fig.layout.height
+        layout_update = {key: value for key, value in layout_update.items() if value is not None}
+        fig.update_layout(layout_update)
+        return fig
+
---
+++ comp/layout.py
@@ -0,0 +1,68 @@
+import dash_bootstrap_components as dbc
+from dash import Output, Input, State, no_update, html, dcc, ctx
+import json
+import dash_ace
+
+def make_layout(fig):
+    return dbc.Container(id='layout-div', children=[
+        dbc.Button('Update Layout', id='update-btn',style={'width':'100%',
+                                                        'margin':'5px',
+                                                        'bordre-radius':'20px'},
+        color='primary', className='mt-2'),
+        dash_ace.DashAceEditor(
+            id='layout-area',
+            theme='light',
+            mode='json', 
+            fontSize=14,
+            showGutter=True,
+            showPrintMargin=False,
+            highlightActiveLine=True,
+            wrapEnabled=False,
+            readOnly=False,
+            width='100%',
+            height='1000px'
+        ),
+    ], style={'margin': '5px', 'display': 'block',"height": "98vh",
+                    "overflowY": "scroll",
+                    'overflowX': 'hidden',
+                    'margin':'5px'})
+
+def register_layout(app, fig):
+    @app.callback(
+        [Output('figure-preview', 'figure', allow_duplicate=True),
+         Output('layout-area', 'value', allow_duplicate=True)],
+        [Input('update-btn', 'n_clicks'),
+         Input('figure-preview', 'figure')],
+        [State('layout-area', 'value')],
+        prevent_initial_call=True  # Prevent initial callback on app start
+    )
+    def update_figure_and_json(n_clicks, figure, layout_json):
+        triggered_id = ctx.triggered_id
+        
+        # Case when figure changes (e.g., from other callbacks or user interactions)
+        if figure and triggered_id == 'figure-preview':
+            layout = figure.get('layout', {})
+            return figure, json.dumps(layout, indent=4)
+        
+        # Case when the button is clicked
+        if n_clicks is not None and triggered_id == 'update-btn':
+            try:
+                # Apply changes from the JSON editor to the figure
+                new_layout = json.loads(layout_json)
+
+                # Ensure the new layout is a valid dictionary
+                if isinstance(new_layout, dict):
+                    # Update the figure with the new layout
+                    updated_fig = {
+                        "data": figure['data'],  # Keep the existing data, only change layout
+                        "layout": new_layout
+                    }
+
+                    # Return the updated figure and the updated JSON layout for the editor
+                    print(fig['data'])
+                    return updated_fig, json.dumps(new_layout, indent=4)
+                else:
+                    return no_update, no_update
+            except json.JSONDecodeError:
+                return no_update, no_update
+        return no_update, no_update
