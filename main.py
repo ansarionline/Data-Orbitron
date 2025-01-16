4da8c0aa49abe5064a8d2d23fc4700e5aec224d3
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-01-17 00:14:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash
 from dash import Output, Input, State, html, dcc
 import dash_bootstrap_components as dbc
+from dash_breakpoints import WindowBreakpoints
 import plotly.subplots as ps
 import plotly.graph_objects as go
 from dash_resizable_panels import PanelGroup, Panel, PanelResizeHandle
@@ -19,10 +20,9 @@ app = dash.Dash('Data Orbitron',
 app.server.secret_key = os.urandom(24)
 server = app.server
 
-app.layout = html.Div(
+app.layout = html.Div([
     PanelGroup(
         id='panel-resizable',
-        direction='horizontal',
         children=[
             dcc.Store(id='tab-session',data={'counter': 0},storage_type='session'),
                     Panel(
@@ -31,16 +31,35 @@ app.layout = html.Div(
                             dbc.Col(
                                 html.Div(
                                     [
-                                        panels.make_panel(app, fig),
+                                        panels.make_panel(app, fig, 'assets/favicon.ico'),
                                         html.Div(id='panel-content', style={'margin-top': '20px'})
                                     ]
                                 ),
-                                style={'height': '100vh', 'margin': '5px'},
+                                style={'height': '100%', 'margin': '5px'},
+                            )
+                        ],
+                    defaultSizePercentage = 30
+                    ),
+                    PanelResizeHandle(
+                        id='handler',
+                        children=html.Div(style={'height': '100%', 'width': '5px','backgroundColor':'#4682B4'})
+                    ),
+                    Panel(
+                        id='panel-layout',
+                        children=[
+                            dbc.Col(
+                                html.Div(
+                                    [
+                                        layout.make_layout(fig),
+                                    ]
+                                ),
+                                style={'height': '100%', 'margin': '5px'},
                             )
                         ]
                     ),
                     PanelResizeHandle(
-                        html.Div(style={'height': '100%', 'width': '5px','backgroundColor':'lightblue'})
+                        id='handler0',
+                        children=html.Div(style={'height': '100%', 'width': '5px','backgroundColor':'lightblue'})
                     ),
                     Panel(
                         id='panel-right',
@@ -52,29 +71,27 @@ app.layout = html.Div(
                                             id='figure-preview',
                                             figure=fig,
                                             config={'displaylogo': False, 'editable': True},
-                                            style={'height': '80vh', 'padding': '10px'}
+                                            style={'height': '700px', 'padding': '10px'}
                                         )
                                     ]
                                 ),
                                 style={'padding': '20px'}
                             )
                         ],
-                        defaultSizePercentage = 75
+                        defaultSizePercentage = 70
                     ),
-                ],
-                style={'margin': '0', 'width': '100%'}
-            )
+                ],style={'margin': '0', 'width': '100%'},
+        direction='horizontal')]
 )
 subplot.register_subplots(app, fig, go)
-data.register_data(app, fig)
-axis.register_axis(app, fig)
-layout.register_layout(app, fig)
+data.register_data(app,fig)
+axis.register_axis(app,fig)
+layout.register_layout(app,fig)
 figure.register_figure(app,fig)
-panels.register_panel(app, 'data-button', 'data-div')
-panels.register_panel(app, 'subplots-button', 'subplots-div')
-panels.register_panel(app, 'axis-button', 'axis-div')
-panels.register_panel(app, 'figure-button', 'figure-div')
-panels.register_panel(app, 'dict-button', 'layout-div')
+panels.register_panel(app,'data-button','data-div')
+panels.register_panel(app,'subplots-button','subplots-div')
+panels.register_panel(app,'axis-button','axis-div')
+panels.register_panel(app,'figure-button','figure-div')
 
 @app.callback(
     Output('tab-session', 'data'),
